7f545bdeed40de465225e9d148d112ea1dc1b32a
Chad Arimura <carimura@gmail.com>
2022-03-07 09:41:18 -0800

    stuff

---
+++ src/dev/java/Hello.java
@@ -0,0 +1,9 @@
+package dev.java;
+public class Hello {
+    public static void main(String args[]) {
+        System.out.println(System.getProperty("java.version"));
+        System.out.println(System.getProperty("java.vendor"));
+        System.out.println(System.getProperty("java.vendor.url"));
+    }
+}
+
